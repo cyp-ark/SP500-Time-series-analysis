3a912758d4894a41ab8e55a5b5dac7173a929c18
nnnhh03 <postno0310@gmail.com>
2025-01-08 15:14:01 +0900

    Create st_NN.py

---
+++ st_NN.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import EDA_NN
+import stock_NN
+
+def sidebar_nagivation():
+    st.session_state["session"]="EDA_NN"
+    if st.sidebar.button('Stock'):
+        st.session_state["session"]="Stock_NN"
+        
+    st.sidebar.title('Options')
+
+def run():
+    st.set_page_config(layout='wide') 
+    
+    # 사이드바 네비게이션
+    sidebar_nagivation()
+    
+    if st.session_state.get("session","Stock") == "Stock_NN":
+        stock_NN.main()
+    if st.session_state.get("session","Stock") == "EDA_NN":
+        st.title("EDA")
+        st.write("EDA is not implemented yet.")
+        EDA_NN.main()
+    
+if __name__ == "__main__":
+    run() 
