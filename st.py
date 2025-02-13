dc2f8c4721e63d7a63ee709c7414f8d383b526e0
ChanyoungPark <chanreverse@gmail.com>
2025-02-13 12:27:57 +0900

    Update st.py

--- st.py
+++ st.py
@@ -1,32 +1,39 @@
 import streamlit as st
 import EDA
-import reg
 import stock
-
-def sidebar_nagivation():
-    st.sidebar.title('Navigation')
-    if st.sidebar.button('EDA'):
-        st.session_state["session"]="EDA"
-    if st.sidebar.button('Stock'):
-        st.session_state["session"]="Stock"
-    if st.sidebar.button('Regression'):
-        st.session_state["session"]="Regression"
-        
-    
+import reg
+ 
+def sidebar_navigation():
     st.sidebar.title('Options')
+  
+    # 사이드바 버튼을 통해 세션 상태 설정
+    if "session" not in st.session_state:
+        st.session_state["session"] = "EDA_Final"  # 초기 세션 상태 설정
+
+    # 항상 버튼이 표시되도록 구현
+    if st.sidebar.button('EDA'):
+        st.session_state["session"] = "EDA_Final"
+    if st.sidebar.button('Stock'):
+        st.session_state["session"] = "Stock_Final"
+    if st.sidebar.button('Regression'):
+        st.session_state["session"] = "Regression_Final"
 
 def run():
-    st.set_page_config(layout='wide')
-    
+    # 페이지 기본 설정
+    st.set_page_config(layout='wide', page_title='Navigation App')
+
     # 사이드바 네비게이션
-    sidebar_nagivation()
-    
-    if st.session_state.get("session","Stock") == "Stock":
+    sidebar_navigation() 
+
+    # 세션 상태에 따라 각 모듈의 main 함수 실행
+    if st.session_state["session"] == "Stock_Final":
         stock.main()
-    elif st.session_state["session"] == "EDA":
+    elif st.session_state["session"] == "EDA_Final":
+        # st.title("EDA")
         EDA.main()
-    elif st.session_state["session"] == "Regression":
+    elif st.session_state["session"] == "Regression_Final":
+        # st.title("Regression")
         reg.main()
-    
+
 if __name__ == "__main__":
     run()
